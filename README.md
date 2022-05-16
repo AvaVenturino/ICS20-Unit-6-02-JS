9df7af5a4c2171d28feb8c6cad1df0200bc7274c
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-16 17:14:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS20-Unit-5-08-JS
+
+[![Ava Venturino's Super Linter](https://github.com/AvaVenturino/ICS20-Unit-5-08-JS/workflows/Ava%20Venturino's%20Super%20Linter/badge.svg)](https://github.com/AvaVenturino/ICS20-Unit-5-08-JS/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/AvaVenturino/ICS20-Unit-5-08-JS)](https://repl.it/github/AvaVenturino/ICS20-Unit-5-08-JS)
+
+This site can be found at: [https://AvaVenturino.github.io/ICS20-Unit-5-08-JS](https://AvaVenturino.github.io/ICS20-Unit-5-08-JS)
---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Ava Venturino All rights reserved
+//
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit-5-08-JS/sw.js", {
+    scope: "/ICS20-Unit-5-08-JS/",
+  });
+}
+
+/**
+ * Input
+ */
+function myButtonClicked() {
+
+  if (localStorage.cookieClicker) {
+    localStorage.cookieClicker = Number(localStorage.cookieClicker) + 1
+  } else {
+    localStorage.cookieClicker = + 1;
+  }
+  document.getElementById("answer").innerHTML =
+    "Total hits : " + localStorage.cookieClicker
+}
